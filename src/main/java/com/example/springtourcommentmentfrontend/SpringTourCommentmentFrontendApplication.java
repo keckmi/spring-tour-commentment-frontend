5d866eba0cb0e8de7cb7edbde171bb74e8068a24
Michael Keck <m.keck@macd8cb8a31cf7a.in.pp-fachinformatik.de>
2022-10-19 13:47:33 +0200

    Initial commit

---
+++ src/main/java/com/example/springtourcommentmentfrontend/SpringTourCommentmentFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtourcommentmentfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTourCommentmentFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTourCommentmentFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springtourcommentmentfrontend/SpringTourCommentmentFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtourcommentmentfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTourCommentmentFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
